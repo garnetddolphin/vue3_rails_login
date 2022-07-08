81883cc8bbd69d756656f86a29de0f1f676a3e81
garnetddolphin <ddolphin.loves.gc@gmail.com>
2022-07-08 17:18:46 +0900

    Create auth controller

---
+++ rails_backend/app/controllers/auth_controller.rb
@@ -0,0 +1,12 @@
+class AuthController < ApplicationController
+  def create
+    user = User.find_by(email: params[:email])
+    token = ''
+    status = :unauthorized
+    if user && user.authenticate(params[:password])
+      token = Session.create(user)
+      status = :created
+    end
+  render json: { token: token }, status: status
+  end
+end
